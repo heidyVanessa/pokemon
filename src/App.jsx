5f73d2e22fa824462dd260236895232829336dc1
heidyVanessa <130875721+heidyVanessa@users.noreply.github.com>
2025-04-07 19:50:47 +0000

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Menu from './componentes/menu'
-import Aleatorios from './componentes/aleatorios';
-import Lista from './componentes/lista';
-import Capturados from './componentes/capturados';
-import Favoritos from './componentes/favoritos';
-import Usuarios from './componentes/usuarios';
-import Detalle from './componentes/detalle';
-
-function App() {
-
-  return (
+import { useState } from 'react' 
+import './App.css' 
+import Aleatorios from './componentes/aleatorios' 
+import Captutados from './componentes/capturados' 
+import Favoritos from './componentes/favoritos' 
+import Lista from './componentes/lista' 
+import Pokemon from './componentes/pokemon' 
+import Usuarios from './componentes/usuarios' 
+import { Route, Router, Routes } from 'react-router-dom'
+function App() { 
+  return ( 
     <Router>
-
-      <Menu />
-      
       <Routes>
-        <Route path="/" element={<Lista />} />
-        <Route path="/usuarios" element={<Usuarios />} />
-        <Route path="/aleatorios" element={<Aleatorios />} />
-        <Route path="/capturados" element={<Capturados />} />
-        <Route path="/favoritos" element={<Favoritos />} />
-        <Route path="/:nombre" element={<Detalle />} />
+        <Route path="/" element={<Aleatorios/>} />
+        <Route path="/" element={<Captutados/>} />
+        <Route path="/" element={<Favoritos/>} />
+        <Route path="/" element={<Lista/>} />
+        <Route path="/" element={<Usuarios/>} />
       </Routes>
-    
-    </Router>
-  );
-}
-
-export default App;
+    </Router>  
+  ) 
+} 
+export default App
